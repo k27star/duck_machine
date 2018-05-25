24de981f729c7a7f024a0f39a67c28aeda8cba22
Kristine Stecker <kristine@Kristines-MacBook-Pro.local>
2018-05-25 09:52:36 -0700

    May 25 no clue what I'm doing

--- assembler_pass1.py
+++ assembler_pass1.py
@@ -1,4 +1,6 @@
 """
+by Kristine Stecker
+
 Assembler for DM2018W assembly language.
 
 This assembler is for fully resolved instructions,
@@ -60,16 +62,19 @@ import sys
 import io
 import re
 import logging
+
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 # Configuration constants
-ERROR_LIMIT = 5    # Abandon assembly if we exceed this
+ERROR_LIMIT = 5  # Abandon assembly if we exceed this
+
 
 # Exceptions raised by this module
 class SyntaxError(Exception):
     pass
+
 
 ###
 # The whole instruction line is encoded as a single
@@ -82,13 +87,14 @@ class SyntaxError(Exception):
 # label without an instruction or an instruction without
 # a label).
 ###
+###
 
 
 # To simplify client code, we'd like to return a dict with
 # the right fields even if the line is syntactically incorrect.
-DICT_NO_MATCH = { 'label': None, 'opcode': None, 'predicate': None,
-                      'target': None, 'src1': None, 'src2': None,
-                      'offset': None, 'comment': None }
+DICT_NO_MATCH = {'label': None, 'opcode': None, 'predicate': None,
+                 'target': None, 'src1': None, 'src2': None,
+                 'offset': None, 'comment': None}
 
 
 ###
@@ -109,6 +115,8 @@ class AsmSrcKind(Enum):
     FULL = auto()
     # A data location, not an instruction
     DATA = auto()
+    # Symbolic label that needs to be resolved
+    SYMBOLIC = auto()
 
 
 # Lines that contain only a comment (and possibly a label).
@@ -154,7 +162,7 @@ ASM_FULL_PAT = re.compile(r"""
    """, re.VERBOSE)
 
 # Defaults for values that ASM_FULL_PAT makes optional
-INSTR_DEFAULTS = [ ('predicate', 'ALWAYS'), ('offset', '0') ]
+INSTR_DEFAULTS = [('predicate', 'ALWAYS'), ('offset', '0')]
 
 # A data word in memory; not a DM2018W instruction
 #
@@ -178,11 +186,33 @@ ASM_DATA_PAT = re.compile(r"""
    \s*$             
    """, re.VERBOSE)
 
+ASM_SYMBOLIC_PAT = re.compile(r"""
+   (
+     (?P<label> [a-zA-Z]\w*):
+   )?
+   # The instruction proper 
+   \s*
+   (
+     (?P<opcode>    (STORE) |(LOAD) |(JUMP) )  # Opcode
+     (/ (?P<predicate> [a-zA-Z]+) )?          # Predicate (optional)
+     \s+
+     ((?P<target>    r[0-9]+),)?            # Optionally one register
+     (?P<symbol>     [a-zA-Z]\w*)               # Symbolic label
+   )
+   # Optional comment follows # or ; 
+   (
+     \s*
+     (?P<comment>[\#;].*)
+   )?       
+   \s*$             
+   """, re.VERBOSE)
 
 PATTERNS = [(ASM_FULL_PAT, AsmSrcKind.FULL),
             (ASM_DATA_PAT, AsmSrcKind.DATA),
-            (ASM_COMMENT_PAT, AsmSrcKind.COMMENT)
+            (ASM_COMMENT_PAT, AsmSrcKind.COMMENT),
+            (ASM_SYMBOLIC_PAT, AsmSrcKind.SYMBOLIC)
             ]
+
 
 def parse_line(line: str) -> dict:
     """Parse one line of assembly code.
@@ -210,66 +240,149 @@ def fill_defaults(fields: dict) -> None:
         if fields[key] == None:
             fields[key] = value
 
-def value_parse(int_literal: str) -> int:
-    """Parse an integer literal that could look like
-    42 or like 0x2a
-    """
-    if int_literal.startswith("0x"):
-        return int(int_literal, 16)
-    else:
-        return int(int_literal, 10)
+# PSEUDOCODE
+# Build a table (dictionary) associating labels and addresses ///
+# (FIRST PASS)
+# examine each line
+# if it has a label, put the label and its address in the dictionary
+# if it is NOT a comment-type line (regex ...)
+#           advance the address counter
 
 
-def assemble(lines: List[str]) -> List[int]:
-    """
-    Simple one-pass assembly for now; must be extended to two
-    passes to write convenient assembly code with labels; probably 
-    to three passes with expansion of control flow into standard 
-    assembly code. 
-    """
-    error_count = 0
-    instructions = [ ]
+# use table
+# (SECOND PASS)
+# examine each line
+# if it IS a SYMBOLIC line (regex ...)
+#           transform it into a resolved line
+#           (WRITE A FUNCTION FOR THIS)
+
+
+#DESIGN NOTES
+
+# FIRST PASS:
+# Build a table (dictionary!)
+#   (label: addr) pairs
+
+# SECOND PASS:
+# go through the lines
+# find ones that match symbolic instruction (regex)
+# resolve them to complete (FULL) asm instructions
+
+
+
+def build_table(lines: List[str]):
+    '''
+    First Pass
+    Build table - dictionary which maps labels to addresses
+    Go through each line
+    If line has a label, put label and its corresponding address
+    in the dictionary
+    If it is not a comment-type line (use regex to figure this out)
+    then increment our address
+
+    # go through the lines
+    # keep track of the address as we go
+            # keep address counter
+            # increment only if the line is NOT a comment
+    # how do I tell if my line is not a comment?
+    # REGEX!!!
+    #apply parse_line, figure out what kind it was
+    '''
+    curr_addr = 0
+    sym_table = { }
+
     for lnum in range(len(lines)):
         line = lines[lnum]
-        log.debug("Processing line {}: {}".format(lnum, line))
-        try: 
+        fields = parse_line(line)
+        #print("Fields at address", curr_addr, "is:", fields)
+        if fields["kind"] != AsmSrcKind.COMMENT:
+            curr_addr += curr_addr
+            print("Recognized a comment!")
+        if fields["kind"] == AsmSrcKind.SYMBOLIC:
+            sym_table['label'] = fields["symbol"]
+            sym_table['position'] = curr_addr
+    return sym_table
+
+
+def transform_lines(lines: List[str], sym_table: dict) -> None:
+    '''
+    # wrapper function that drives second pass
+
+    # second pass, iterateing over all the lines again
+    in order to calculate the "pc-relative-address" for resolving
+    - need to know:
+    where we are in our program
+    (i.e. another address counter will show up here)
+    '''
+    error_count = 0
+    curr_addr = 0
+
+    for lnum in range(len(lines)):
+        line = lines[lnum]
+        try:
             fields = parse_line(line)
-            #FIXME I added this
+            #this is a handy one to print
             print(fields)
-            if fields["kind"] == AsmSrcKind.FULL:
-                log.debug("Constructing instruction")
-                fill_defaults(fields)
-                instr = instruction_from_dict(fields)
-                word = instr.encode()
-                instructions.append(word)
-            elif fields["kind"] == AsmSrcKind.DATA:
-                word = value_parse(fields["value"])
-                instructions.append(word)
+            if fields["kind"] != AsmSrcKind.COMMENT:
+                curr_addr += curr_addr
+            if fields["kind"] == AsmSrcKind.FULL or AsmSrcKind.SYMBOLIC:
+                new_line = resolve_line(fields, sym_table, curr_addr)
+                # need to write new line to file, but how???
+                print(new_line,file=args.objfile)
             else:
-                log.debug("No instruction on line")
+                log.debug("Problem trying to build table")
         except SyntaxError as e:
             error_count += 1
             print("Syntax error in line {}: {}".format(lnum, line))
         except KeyError as e:
             error_count += 1
-            print("Unknown word in line {}: {}".format(lnum, e))
+            print("Unknown label in line {}: {}".format(lnum, e))
         except Exception as e:
             error_count += 1
             print("Exception encountered in line {}: {}".format(lnum, e))
         if error_count > ERROR_LIMIT:
             print("Too many errors; abandoning")
             sys.exit(1)
-    return instructions
+
+    return None
+
+def resolve_line(fields: dict, curr_addr: int, sym_table: dict) -> str:
+    #FIXME
+    # used by transform_lines function
+    # fiddly string format stuff happens
+    new_line = ''
+    if fields["symbol"] in sym_table:
+        fields["offset"] = curr_addr + sym_table['position']
+        # do I need an opcode in here somewhere?
+        # what is opcode again? lol
+        if fields["opcode"] == 'LOAD' or 'STORE':
+            new_line = "{}/{} {},{},r15[{}] " \
+                       "# Access variable '{}'".format(
+                        fields["opcode"], fields["predicate"],
+                        fields["target"], fields["src1"],
+                        fields["offset"], fields["comment"],
+                        sym_table['label'])
+        elif fields["opcode"] == 'JUMP':
+            fields["opcode"] = 'ADD'
+            new_line = "{} {},{},{}[{}] " \
+                       "# Jump to {}".format(
+                        fields["opcode"], fields["target"],
+                        fields["src1"], fields["src2"],
+                        fields["offset"], fields["comment"],
+                        sym_table['label'])
+
+    return new_line
+
 
 def cli() -> object:
     """Get arguments from command line"""
     parser = argparse.ArgumentParser(description="Duck Machine Assembler (pass 2)")
     parser.add_argument("sourcefile", type=argparse.FileType('r'),
-                            nargs="?", default=sys.stdin,
-                            help="Duck Machine assembly code file")
-    parser.add_argument("objfile", type=argparse.FileType('w'),
-                            nargs="?", default=sys.stdout, 
-                            help="Object file output")
+                        nargs="?", default=sys.stdin,
+                        help="Duck Machine assembly code file")
+    parser.add_argument("resolved", type=argparse.FileType('w'),
+                        nargs="?", default=sys.stdout,
+                        help="Resolved output")
     args = parser.parse_args()
     return args
 
@@ -278,13 +391,10 @@ def main():
     """"Assemble a Duck Machine program"""
     args = cli()
     lines = args.sourcefile.readlines()
-    object_code = assemble(lines)
-    log.debug("Object code: \n{}".format(object_code))
-    for word in object_code:
-        log.debug("Instruction word {}".format(word))
-        print(word,file=args.objfile)
+    table = build_table(lines)
+    lines = transform_lines(lines, table)
+
 
 if __name__ == "__main__":
     main()
-
 
--- cpu.py
+++ cpu.py
@@ -1,5 +1,6 @@
 """
 Duck Machine model DM2018S CPU
+By: Kristine Stecker
 """
 
 from instr_format import Instruction, OpCode, CondFlag, decode
@@ -24,15 +25,84 @@ class CPUStep(MVCEvent):
         self.instr_word = instr_word
         self.instr = instr
 
-# Create a class CPU, subclassing MVCListenable.
-# It should have 16 registers (a list of Register objects),
-# and the first of them should be the special ZeroRegister
-# object that is always zero regardless of what is stored.
-# It should have a CondFlag with the current condition.
-# It should have a boolean "Halted" flag, and execution of
-# the "run" method should halt with the Halted flag is True
-# (set by the HALT instruction). The CPU does not contain
-# the memory, but has a connection to a Memory object
-# (specifically a MemoryMappedIO object).
-# See the project web page for more guidance.
+
+class CPU(MVCListenable):
+    '''
+    Contains 16 registers (register 0 will always be of type
+    ZeroRegister), an ALU object, an interface to memory,
+    and sequential logic for executing programs that may
+    have loops and conditional branches.
+    '''
+
+    def __init__(self, memory):
+        super().__init__()
+
+        self.memory = memory
+        self.registers = [ZeroRegister(), Register(), Register(), Register(), Register(),
+                          Register(), Register(), Register(), Register(), Register(),
+                          Register(), Register(), Register(), Register(), Register(), Register()]
+        self.cond_flag = CondFlag.ALWAYS
+        self.halted = False
+        self.program_pointer = self.registers[15]
+        self.alu = ALU()
+
+    def step(self) -> None:
+        '''Fetches instructions in memory. Decodes instruction word. Determines if
+        instruction should be executed or skipped. Executes if applicable.
+        '''
+        #fetch
+        address = self.program_pointer.get()
+        instruction_word = self.memory.get(address)
+
+        #decode
+        decoded_word = decode(instruction_word)
+
+        self.notify_all(CPUStep(self, address, instruction_word, decoded_word))
+
+        #execute
+        predicate = self.cond_flag & decoded_word.cond
+
+        if predicate:
+            # get values for source registers
+            val1 = self.registers[decoded_word.reg_src1].get()
+            val2 = self.registers[decoded_word.reg_src2].get()
+
+            # add offset to register
+            val2 =  val2 + decoded_word.offset
+
+            #increment program counter
+            self.program_pointer.put(self.program_pointer.get() + 1)
+
+            #sending op code to ALU to execute with 2 values
+            result, self.cond_flag = self.alu.exec(decoded_word.op, val1, val2)
+
+            if decoded_word.op == OpCode.HALT:
+                self.halted = True
+
+            #load
+            elif decoded_word.op == OpCode.LOAD:
+                self.registers[decoded_word.reg_target].put(self.memory.get(result))
+
+            #store
+            elif decoded_word.op == OpCode.STORE:
+                self.memory.put(result, self.registers[decoded_word.reg_target].get())
+
+            else:
+                self.registers[decoded_word.reg_target].put(result)
+        else:
+            self.program_pointer.put(self.program_pointer.get() + 1)
+
+    def run(self, from_addr=0, single_step=False) -> None:
+        ''' Calls step method until it executes the HALT instruction.
+        Allows the option of a single-step mode for debugging.
+        '''
+        self.program_pointer.put(from_addr)
+
+        step_count = 0
+
+        while not self.halted:
+            if single_step:
+                input("Step {}; press enter".format(step_count))
+            self.step()
+            step_count += 1
 
